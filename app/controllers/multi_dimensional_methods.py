Fix bug with selectbox and gradient descent

app/controllers/multi_dimensional_methods.py
@@ -16,7 +16,9 @@ def coordinate_descent(f: AbstractFunction, x, config: DictConfig):
     :return: minimum of function
     """
     step = config['step']
-    epsilon = np.array([[config['epsilon'], config['epsilon']]])
+    eps = config['epsilon']
+    epsilon = np.array([[eps],
+                        [eps]])
     iteration = 0
     while True:
         x_prev = np.copy(x)
@@ -34,7 +36,7 @@ def coordinate_descent(f: AbstractFunction, x, config: DictConfig):
                 iteration += 1
 
         if all(np.abs(x - x_prev) < epsilon):
-            print(f"Iterations: {iteration}")
+            log.info(f"Coordinate descent number of iterations: {iteration}")
             return x
 
 
@@ -47,26 +49,32 @@ def gradient_descent(f: AbstractFunction, x, config: DictConfig):
     :return: minimum of function
     """
     step = config['step']
-    epsilon = np.array([[config['epsilon'], config['epsilon']]])
-    iteration = 0
+    eps = config['epsilon']
+    epsilon = np.array([[eps],
+                        [eps]])
+    step, iteration = 0, 0
+    i = 0
+    mx = 100
     while True:
+        step = 1 / min(i + 1, mx)
+        i += 1
         x_prev = np.copy(x)
         fx_prev = f(x_prev)
         grad_fx = f.grad(x)
         x -= step * grad_fx
         fx = f(x)
         while fx < fx_prev:
+            iteration += 1
             x -= step * grad_fx
             fx_prev = fx
             fx = f(x)
-            iteration += 1
 
         if all(np.abs(x - x_prev) < epsilon):
-            print(f"Iterations: {iteration}")
+            log.info("Gradient descent number of iterations: ", iteration)
             return x
 
 
-def newton_descent(f: AbstractFunction, H, x, config: DictConfig):
+def newton_descent(f: AbstractFunction, x, config: DictConfig):
     """
     Newton Descent
     :param f: function
@@ -76,13 +84,15 @@ def newton_descent(f: AbstractFunction, H, x, config: DictConfig):
     :return: minimum of function
     :return: minimum of function
     """
-    epsilon = np.array([[config['epsilon'], config['epsilon']]])
+    eps = config['epsilon']
+    epsilon = np.array([[eps],
+                        [eps]])
     iteration = 0
     while True:
         x_prev = np.copy(x)
-        x = x - np.linalg.inv(H(x)).dot(f.grad(x))
+        x = x - np.linalg.inv(f.h(x)).dot(f.grad(x))
         iteration += 1
 
         if all(np.abs(x - x_prev) < epsilon):
-            print(f"Iterations: {iteration}")
+            log.info(f"Newton descent number of iterations: {iteration}")
             return x

app/models/multi_dimensional_functions.py
@@ -1,4 +1,4 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 import numpy as np
 
@@ -6,15 +6,20 @@ from math import sqrt, cos, sin, cosh, sinh, pi
 
 alpha = pi / 6
 
+A = 1
+B = 2
+
 
 class AbstractFunction(ABC):
     def __init__(self, x0, y0):
         self.x0 = x0
         self.y0 = y0
 
+    @abstractmethod
     def __call__(self, *x):
         pass
 
+    @abstractmethod
     def grad(self, coords):
         pass
 
@@ -33,21 +38,34 @@ class QuadraticFunction(AbstractFunction):
         return np.array([[2 * (float(coords[0] - self.x0))], [2 * (float(coords[1] - self.y0))]])
 
 
-class HyperbolicCosineFunction(AbstractFunction):
+class CosineFunction(AbstractFunction):
     def __init__(self, x0=1, y0=2):
-        super(HyperbolicCosineFunction, self).__init__(x0, y0)
+        super(CosineFunction, self).__init__(x0, y0)
 
-    def __call__(self, *coords):
-        x_new = self.__calculate_x_new(coords)
-        return np.array([[sinh(x_new[0]) * cos(alpha)], [sinh(x_new[1]) * sin(alpha)]])
+    def __call__(self, x: np.matrix):
+        x_new = np.matrix(
+            [[(x[0, 0] - self.x0) * cos(alpha) + (x[1, 0] - self.y0) * sin(alpha)],
+             [(x[1, 0] - self.y0) * cos(alpha) - (x[0, 0] - self.x0) * sin(alpha)]],
+            float)
+        return A * (x_new[0]) ** 2 + B * (x_new[1]) ** 2
 
-    def grad(self, coords):
-        x_new = self.__calculate_x_new(coords)
-        return np.array([[sinh(x_new[0]) * cos(alpha)], [sinh(x_new[1]) * sin(alpha)]])
+    def grad(self, x):
+        x_new = np.matrix(
+            [[2 * A * cos(alpha) * ((x[0, 0] - self.x0) * cos(alpha) + (x[1, 0] - self.y0) * sin(alpha)) -
+              2 * B * sin(alpha) * ((x[1, 0] - self.y0) * cos(alpha) - (x[0, 0] - self.x0) * sin(alpha))],
 
+             [2 * A * sin(alpha) * ((x[0, 0] - self.x0) * cos(alpha) + (x[1, 0] - self.y0) * sin(alpha))
+              + 2 * B * cos(alpha) * ((x[1, 0] - self.y0) * cos(alpha) - (x[0, 0] - self.x0) * sin(alpha))]],
+            float)
+
+        return np.array([[x_new[0, 0]], [x_new[1, 0]]])
+
+    # матрица Гессе
     def h(self, coords):
-        x_new = self.__calculate_x_new(coords)
-        return np.array([[cosh(x_new[0]) * cos(alpha) ** 2, 0.], [0., cosh(coords[1]) * sin(alpha) ** 2]])
+        return np.array([[2 * A * (cos(alpha)) ** 2 + 2 * B * (sin(alpha)) ** 2,
+                          2 * (cos(alpha)) * (sin(alpha)) * (A - B)],
+                         [2 * (cos(alpha)) * (sin(alpha)) * (A - B),
+                          2 * A * (sin(alpha)) ** 2 + 2 * B * (cos(alpha)) ** 2]])
 
     def __calculate_x_new(self, coords):
         coords = np.array(coords)
@@ -57,9 +75,4 @@ class HyperbolicCosineFunction(AbstractFunction):
                           [(coords[1, 0] - y0) * sin(alpha) - (coords[0, 0] - x0) * cos(alpha)]], float)
 
     def __repr__(self):
-        # !FIXME
-        return f"Hyperbolic"
-
-
-def f(x, y):
-    return np.sin(np.sqrt(x ** 2 + y ** 2))
+        return f"Cosine"

app/views/multidimensional_examples.py
@@ -4,7 +4,7 @@ import hydra
 import streamlit as st
 from omegaconf import DictConfig
 
-from app.models.multi_dimensional_functions import QuadraticFunction, HyperbolicCosineFunction, AbstractFunction
+from app.models.multi_dimensional_functions import QuadraticFunction, CosineFunction, AbstractFunction
 from app.controllers.multi_dimensional_methods import coordinate_descent, gradient_descent, newton_descent
 
 import numpy as np
@@ -15,31 +15,26 @@ log = logging.getLogger(__name__)
 
 def two_argument_form(config: DictConfig):
     st.title("Functions of two and more arguments")
-    methods = [coordinate_descent, gradient_descent, newton_descent]
-    method = st.selectbox("Optimization method", methods, format_func=lambda x: x.__name__)
+    methods = {'coordinate descent': coordinate_descent,
+               'gradient descent': gradient_descent,
+               'newton descent': newton_descent}
 
-    functions_to_optimize = {"quadratic": QuadraticFunction(), "hyperbolic": HyperbolicCosineFunction()}
-    function_to_optimize = st.selectbox("Function to optimize", functions_to_optimize.keys())
-    # st.write(f"minimum:{method((0.,0.), f, config)}")
-    st.pyplot(create_plot(functions_to_optimize[function_to_optimize]))
+    method = methods[st.selectbox("Optimization method", methods)]
 
+    # functions_to_optimize = {"quadratic": QuadraticFunction(), "hyperbolic": CosineFunction()}
+    functions_to_optimize = {
+        "Cosine (1, 2)": CosineFunction(),
+        "Cosine (5, 1)": CosineFunction(5, 1)
+    }
+    function_to_optimize = functions_to_optimize[st.selectbox("Function to optimize", functions_to_optimize.keys())]
 
-def create_plot(function: AbstractFunction):
-    x1 = np.linspace(-12, 12, 30)
-    x2 = np.linspace(-12, 12, 30)
-    x1, x2 = np.meshgrid(x1, x2)
-    function_values = function(x1, x2)
+    start_points = {
+        "(-1, -2)": np.matrix([[-1.], [-2.]]),
+        "(-4, -5)": np.matrix([[-4.], [-5.]])
+    }
+    x = start_points[st.selectbox("Start point", start_points)]
 
-    fig = plt.figure()
-    ax = plt.axes(projection='3d')
-
-    ax.plot_surface(x1, x2, function_values, rstride=1, cstride=1,
-                    cmap='viridis', edgecolor='none')
-    ax.set_xlabel('x1')
-    ax.set_ylabel('x2')
-    ax.set_zlabel('f')
-
-    return fig
+    st.write(f"minimum:{method(f=function_to_optimize, x=x, config=config)}")
 
 
 if __name__ == '__main__':